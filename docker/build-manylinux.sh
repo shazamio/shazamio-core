2b8eabf53f4be82b827b3958a6dcef10acb61231
Danipulok <danipulok@gmail.com>
2026-09-04 23:40:44 +0000

    refactor(docker): drop the dead `PYTHON_INTERPRETER` build argument

--- docker/build-manylinux.sh
+++ docker/build-manylinux.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [[ -n $PYTHON_INTERPRETER ]]; then
-    maturin build -i "$PYTHON_INTERPRETER" --release --out dist
-    exit 0
-fi
-
 maturin build --release --out dist
 
 # `abi3` covers only CPython. PyPy has no stable ABI, so it is left out of that
